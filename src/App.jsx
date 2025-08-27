d7cba4a1aa173c39d71214ac0a0d6d3941ba07f6
Kath <e.i.ionvva@gmail.com>
2025-08-27 23:24:09 +0300

    add taskList

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,58 @@
 import { useState } from 'react';
 
 import Sidebar from "./components/Sidebar/Sidebar";
-import Header from "./components/Header/Header"
+import Header from "./components/Header/Header";
+import TaskList from './components/TaskList/TaskList';
 import { habitsData } from './data/habits';
 
-import style from "./App.module.scss"
+import style from "./App.module.scss";
 
 function App() {
-  const [ habits ] = useState(habitsData);
+  const [ habits, setHabits ] = useState(habitsData);
   const [currentHabitId, setCurrentHabitId] = useState(habitsData[0].id);
   const currentHabit = habits.find(h => h.id === currentHabitId);
+
+  const updateTask = (taskId, updates) => {
+    setHabits(prev =>
+      prev.map(habit =>
+        habit.id === currentHabitId
+          ? {
+              ...habit,
+              tasks: habit.tasks.map(t =>
+                t.id === taskId ? { ...t, ...updates } : t
+              ),
+            }
+          : habit
+      )
+    );
+  };
+
+  const deleteTask = (taskId) => {
+    setHabits(prev =>
+      prev.map(habit =>
+        habit.id === currentHabitId
+          ? {
+              ...habit,
+              tasks: habit.tasks.filter(t => t.id !== taskId),
+            }
+          : habit
+      )
+    );
+  };
+
+  const addDay = (newTask) => {
+    setHabits(prev =>
+      prev.map(habit =>
+        habit.id === currentHabitId
+          ? {
+              ...habit,
+              tasks: [...habit.tasks, newTask],
+            }
+          : habit
+      )
+    );
+  };
+  
 
   return (
     <div className={style.page}>
@@ -20,6 +63,13 @@ function App() {
       />
       <main className={style.main}>
         <Header habit = {currentHabit}/>
+        <hr className={style.hr}/>
+        <TaskList 
+          habit={currentHabit}
+          updateTask={updateTask}
+          deleteTask={deleteTask}
+          addDay={addDay}
+        />
       </main>
     </div>
   );
--- src/data/habits.jsx
+++ src/data/habits.jsx
@@ -2,6 +2,30 @@ import { DumbbellIcon } from "../../public/habits/iconsSVG";
 import { WaterIcon } from "../../public/habits/iconsSVG";
 
 export const habitsData = [
-  { id: 1, name: 'Отжимания', goal: '50 раз за месяц', icon: DumbbellIcon },
-  { id: 2, name: 'Питье', goal: '2 литра в день', icon: WaterIcon },
+  { 
+    id: 1, 
+    name: 'Отжимания', 
+    goal: '50 раз за месяц', 
+    icon: DumbbellIcon,
+    tasks: [
+      {
+        id: 101,
+        day: 'День 1',
+        comment: 'Первый день дался тяжело',
+        compiled: true
+      },
+      {
+        id: 102,
+        day: 'День 2',
+        comment: '',
+        compiled: false
+      }
+    ]
+  },
+  { 
+    id: 2, 
+    name: 'Питье', 
+    goal: '2 литра в день', 
+    icon: WaterIcon,
+  },
 ];
